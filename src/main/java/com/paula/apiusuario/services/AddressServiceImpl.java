4a71ff2eac1a0fea46bbbb7c21a274f22c9f6d75
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-08-08 18:04:33 -0300

    feat: implements the addAddress method in AddressServiceImpl class

--- src/main/java/com/paula/apiusuario/services/AddressServiceImpl.java
+++ src/main/java/com/paula/apiusuario/services/AddressServiceImpl.java
@@ -2,12 +2,16 @@ package com.paula.apiusuario.services;
 
 import com.paula.apiusuario.domain.Address;
 import com.paula.apiusuario.exceptions.AddressNotFoundException;
+import com.paula.apiusuario.repositories.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AddressServiceImpl implements AddressService {
 
+    @Autowired
+    private AddressRepository addressRepository;
 
     @Override
     public Address findAddress(String cep) throws AddressNotFoundException {
@@ -22,6 +26,6 @@ public class AddressServiceImpl implements AddressService {
 
     @Override
     public void addAddress(Address address) {
-
+        addressRepository.save(address);
     }
 }
