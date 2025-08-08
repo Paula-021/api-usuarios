6360b1062934d207c16a9e62f0a79e991ed7025c
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-08-08 17:55:24 -0300

    feat: implements AddressNotFoundException, in the findAddress method it checks if the address is null and throws the exception

--- src/main/java/com/paula/apiusuario/services/AddressService.java
+++ src/main/java/com/paula/apiusuario/services/AddressService.java
@@ -1,9 +1,10 @@
 package com.paula.apiusuario.services;
 
 import com.paula.apiusuario.domain.Address;
+import com.paula.apiusuario.exceptions.AddressNotFoundException;
 
 public interface AddressService {
-    Address findAddress(String cep);
+    Address findAddress(String cep) throws AddressNotFoundException;
 
     void addAddress(Address address);
 }
--- src/main/java/com/paula/apiusuario/services/AddressServiceImpl.java
+++ src/main/java/com/paula/apiusuario/services/AddressServiceImpl.java
@@ -1,16 +1,22 @@
 package com.paula.apiusuario.services;
 
 import com.paula.apiusuario.domain.Address;
+import com.paula.apiusuario.exceptions.AddressNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class AddressServiceImpl implements AddressService {
 
 
     @Override
-    public Address findAddress(String cep) {
+    public Address findAddress(String cep) throws AddressNotFoundException {
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://viacep.com.br/ws/" + cep + "/json";
         Address address = restTemplate.getForObject(url, Address.class); // Jackson converte o JSON para o objeto Address
+        if(address == null || address.getCep() == null || address.getCep().isEmpty()) {
+            throw new AddressNotFoundException("Endereço não encontrado para o CEP: " + cep);
+        }
         return address;
     }
 
