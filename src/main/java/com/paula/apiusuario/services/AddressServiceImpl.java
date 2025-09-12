f67650c1b7967e8a338c41c4b584a18c5e6659bd
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-09-12 14:53:48 -0300

    feat: add updateAddressComplementary method to AddressServiceImpl for updating address fields

--- src/main/java/com/paula/apiusuario/services/AddressServiceImpl.java
+++ src/main/java/com/paula/apiusuario/services/AddressServiceImpl.java
@@ -1,6 +1,7 @@
 package com.paula.apiusuario.services;
 
 import com.paula.apiusuario.domain.Address;
+import com.paula.apiusuario.endpoints.dtos.AddressRequestDTO;
 import com.paula.apiusuario.exceptions.AddressNotFoundException;
 import com.paula.apiusuario.repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,4 +32,31 @@ public class AddressServiceImpl implements AddressService {
     public void addAddress(Address address) {
         addressRepository.save(address);
     }
+
+    @Override
+    public void updateAddress(Address address) {
+        addressRepository.save(address);
+    }
+
+    @Override
+    public void updateAddressComplementary(Long id, AddressRequestDTO addressRequestDTO) throws AddressNotFoundException {
+        //Address ids: 1, 2, 3, 4, 5
+        //busca o id do endereço informado e valida se ele existe
+        Address address = addressRepository.findById(id).orElseThrow(() -> new AddressNotFoundException("Address not found with ID: " + id));
+        //validar se o usuário digitou informações para os campos complementares
+        //atualiza os campos complementares do endereço
+        if(!addressRequestDTO.getLogradouro().isEmpty()){
+            address.setLogradouro(addressRequestDTO.getLogradouro());
+        }
+        if(!addressRequestDTO.getComplemento().isEmpty()){
+            address.setComplemento(addressRequestDTO.getComplemento());
+        }
+        if(!addressRequestDTO.getBairro().isEmpty()){
+            address.setBairro(addressRequestDTO.getBairro());
+        }
+
+
+        addressRepository.save(address);
+    }
+
 }
